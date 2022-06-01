1c05b09f27f72a57bcbb757dd46d3e6d0a5857a3
Allan <allangoncalves.adm@gmail.com>
2022-06-01 17:05:28 -0300

    Consume Github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,24 @@
 import { RepositoryItem } from './RepositoryItem';
-
-const repository = {
-	name: 'Repository name',
-	description: 'Repository description',
-	link: 'https://github.com/angular/angular',
-};
+import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
 
 export function RepositoryList() {
+	const [repositories, setRepositories] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/orgs/rocketseat/repos')
+			.then(response => response.json())
+			.then(data => setRepositories(data));
+	}, []);
+
 	return (
 		<section className="repository-list">
 			<h1>Repository list</h1>
 			<br />
 			<ul>
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
+				{repositories.map(repository => {
+					return <RepositoryItem key={repository.id} repository={repository} />;
+				})}
 			</ul>
 			<br />
 		</section>
